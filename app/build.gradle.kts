7d57c3768928a1b3a1b828754606761c6082f7a4
mert025 <69677395+mert025@users.noreply.github.com>
2026-04-05 14:27:05 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.batterynotifier"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.example.batterynotifier"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
     }
@@ -29,6 +29,9 @@ android {
     }
     buildFeatures {
         viewBinding = true
+    }
+    lint {
+        abortOnError = false
     }
 }
 
